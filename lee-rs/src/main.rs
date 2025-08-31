ffef509d634bb26f3e6fbc62803a8b91c4575a37
HapooIsLuv <yeonjun.thai@gmail.com>
2025-08-31 16:16:42 +0700

    added Axum server to lee-rs

--- lee-rs/src/main.rs
+++ lee-rs/src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router
+};
+
+use serde::{Deserialize, Serialize};
+
+
+#[tokio::main]
+async fn main() { 
+    let server = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, server).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello world!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
